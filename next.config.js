7ceb8b2306158c0960cf416e0ef9571f081390e6
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-12-28 22:16:00 +0530

    addedFiles

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(pdf)$/,
+      type: 'asset/resource',
+    });
+
+    return config;
+  },  
 }
 
 module.exports = nextConfig
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Header from "@/components/Header/Header";
+import AboutComponent from "@/components/About/AboutComponent";
+import Footer from "@/components/Footer/Footer";
+import TimeLine from "@/components/About/TimeLine";
+const About = () => {
+    return (
+        <>
+            <Header active="About" />
+            <div className="bg-black w-full overflow-scroll">
+            <div className=" mt-20">
+            <AboutComponent />
+            </div>
+            <div>
+                <TimeLine/>
+            </div>
+            <Footer></Footer>
+            </div>
+        </>
+    )
+};
+export default About;
